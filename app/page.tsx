3eb053bdf2938ce2ad644e8429f78ffcc0f0444e
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-08-04 00:29:29 +0100

    Get sign-in with Google working.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { SignIn } from "@/components/sign-in";
 
 export default function Home() {
   return (
@@ -26,6 +27,7 @@ export default function Home() {
             />
           </a>
         </div>
+        <SignIn />
       </div>
 
       <div className={styles.center}>
