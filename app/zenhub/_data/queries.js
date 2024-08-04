eca91a22bd2ee4e0ed9463d852f17add52f8dcb4
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-08-04 18:21:38 +0100

    Copy ZH data fetching code from https://github.com/techanvil/zenhub-dependency-graph/.

---
+++ app/zenhub/_data/queries.js
@@ -0,0 +1,189 @@
+/**
+ * Initially copied from https://github.com/techanvil/zenhub-dependency-graph/blob/main/src/data/queries.js
+ *
+ * TODO: Refactor to a shared library.
+ */
+
+export const GET_WORKSPACE_QUERY = gql`
+  query GetWorkSpace($workspaceName: String!) {
+    viewer {
+      searchWorkspaces(query: $workspaceName) {
+        nodes {
+          id
+          name
+          zenhubOrganization {
+            name
+          }
+          activeSprint {
+            # id
+            name
+          }
+          sprints(filters: { state: { eq: OPEN } }) {
+            nodes {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_REPO_AND_PIPELINES_QUERY = gql`
+  query GetRepoAndPipelines($workspaceId: ID!) {
+    workspace(id: $workspaceId) {
+      defaultRepository {
+        id
+        ghId
+      }
+      pipelinesConnection {
+        nodes {
+          id
+        }
+      }
+    }
+  }
+`;
+
+export const GET_EPIC_LINKED_ISSUES_QUERY = gql`
+  query GetEpicLinkedIssues(
+    $workspaceId: ID!
+    $repositoryId: ID!
+    $repositoryGhId: Int!
+    $epicIssueNumber: Int!
+    $pipelineIds: [ID!]!
+  ) {
+    linkedIssues: searchIssues(
+      workspaceId: $workspaceId
+      epicIssueByInfo: {
+        repositoryGhId: $repositoryGhId
+        issueNumber: $epicIssueNumber
+      }
+      includeClosed: true
+      filters: { repositoryIds: [$repositoryId], pipelineIds: $pipelineIds }
+    ) {
+      nodes {
+        number
+        title
+        htmlUrl
+        state
+        assignees {
+          nodes {
+            login
+            # name
+          }
+        }
+        blockingIssues {
+          nodes {
+            number
+          }
+        }
+        blockedIssues {
+          nodes {
+            number
+          }
+        }
+        pipelineIssue(workspaceId: $workspaceId) {
+          pipeline {
+            name
+          }
+        }
+        estimate {
+          value
+        }
+        sprints {
+          nodes {
+            # id
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ISSUE_BY_NUMBER_QUERY = gql`
+  query GetIssueByNumber(
+    $workspaceId: ID!
+    $repositoryGhId: Int!
+    $issueNumber: Int!
+  ) {
+    issueByInfo(issueNumber: $issueNumber, repositoryGhId: $repositoryGhId) {
+      number
+      title
+      htmlUrl
+      state
+      assignees {
+        nodes {
+          login
+          # name
+        }
+      }
+      pipelineIssue(workspaceId: $workspaceId) {
+        pipeline {
+          name
+        }
+      }
+      blockingIssues {
+        nodes {
+          number
+        }
+      }
+      blockedIssues {
+        nodes {
+          number
+        }
+      }
+      sprints {
+        nodes {
+          # id
+          name
+        }
+      }
+      estimate {
+        value
+      }
+    }
+  }
+`;
+
+export const GET_ALL_ORGANIZATIONS = gql`
+  query GetAllOrganizations {
+    viewer {
+      zenhubOrganizations {
+        nodes {
+          id
+          name
+          workspaces {
+            nodes {
+              id
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ALL_EPICS = gql`
+  query GetAllEpics($workspaceId: ID!) {
+    workspace(id: $workspaceId) {
+      epics {
+        nodes {
+          issue {
+            number
+            title
+            closedAt
+          }
+        }
+      }
+    }
+  }
+`;
+
+// For the sake of syntax highlighting:
+function gql(strings) {
+  return strings[0];
+}
