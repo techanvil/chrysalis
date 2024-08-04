3971b6cf4e86b3420c9c637d21efeec20b213b22
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-08-04 01:02:51 +0100

    Add a note re. locking down sign-in to a domain.

--- auth.ts
+++ auth.ts
@@ -3,4 +3,22 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  /*
+  // Lock down the sign-in to only allow users from a specific domain.
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (!account || !profile?.email) {
+        return false;
+      }
+
+      if (account.provider === "google") {
+        return (
+          !!profile.email_verified && profile.email.endsWith("@techanvil.co.uk")
+        );
+      }
+
+      return false; // Do different verification when there are other providers that don't have `email_verified`. Return `false` while there are no other providers.
+    },
+  },
+  */
 });
