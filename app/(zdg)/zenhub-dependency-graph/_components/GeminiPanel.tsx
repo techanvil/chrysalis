5935611330d71bd9980e271d7358059baf97f014
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-08-17 02:26:04 +0100

    Hook up message box to provide chat capability.

--- app/(zdg)/zenhub-dependency-graph/_components/GeminiPanel.tsx
+++ app/(zdg)/zenhub-dependency-graph/_components/GeminiPanel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { useSession } from "next-auth/react";
@@ -10,7 +10,9 @@ import { submitEpicQuery, EpicChatEntry } from "../_actions/submit-epic-query";
 import { useDebouncedCallback } from "use-debounce";
 import styles from "./GeminiPanel.module.css";
 
-export function GeminiPanel({ graphData }) {
+type GraphData = object[]; // TODO: Fix type.
+
+export function GeminiPanel({ graphData }: { graphData: GraphData }) {
   const session = useSession();
   if (!session) {
     return <p>⚠️ Please sign in to use this feature.</p>;
@@ -20,26 +22,37 @@ export function GeminiPanel({ graphData }) {
     return <p>⚠️ No graph data available.</p>;
   }
 
-  const [latestChatEntry, setLatestChatEntry] = useState<EpicChatEntry | null>(
-    null
-  );
+  const [chatHistory, setChatHistory] = useState<EpicChatEntry[]>([]);
+  const [submittingQuery, setSubmittingQuery] = useState(false);
 
-  // TODO: Fix epicGraphData type.
-  const queryEpic = useDebouncedCallback(async (epicGraphData) => {
-    setLatestChatEntry(null);
+  async function queryEpic(epicGraphData: GraphData, query: string) {
+    setSubmittingQuery(true);
 
     console.log("graphData", epicGraphData);
 
-    const query = ""; // TODO: Get query from user input.
-
     const chatEntry = await submitEpicQuery(graphData, query);
 
-    setLatestChatEntry(chatEntry);
-  }, 1000);
+    setChatHistory((currentChatHistory) => [...currentChatHistory, chatEntry]);
+    setSubmittingQuery(false);
+  }
+
+  const debouncedQueryEpic = useDebouncedCallback(queryEpic, 1000);
 
   useEffect(() => {
-    queryEpic(graphData);
-  }, [graphData, queryEpic]);
+    if (chatHistory.length === 0) {
+      debouncedQueryEpic(graphData, "");
+    } else {
+      setChatHistory([
+        ...chatHistory,
+        {
+          userName: "System",
+          query: "",
+          response: "🔄 graph data updated...",
+        },
+      ]);
+    }
+  }, [graphData]); // Omit debouncedQueryEpic to avoid infinite loop.
+  // }, [graphData, debouncedQueryEpic]);
 
   // useEffect(() => {
   //   (async () => {
@@ -52,25 +65,68 @@ export function GeminiPanel({ graphData }) {
   //   })();
   // }, [graphData]);
 
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleSendMessage() {
+    if (textareaRef.current) {
+      const query = textareaRef.current.value;
+      if (query.trim() === "") {
+        return;
+      }
+
+      setChatHistory([
+        ...chatHistory,
+        {
+          userName: session.data?.user?.name,
+          query: "",
+          response: query, // TODO: Use a simplified type for chat entries.
+        },
+      ]);
+
+      queryEpic(graphData, query);
+      textareaRef.current.value = "";
+    }
+  }
+
   return (
     <div className={`${styles.container} zdg-chat-container`}>
-      {!latestChatEntry && <p>⏳ loading...</p>}
-      {latestChatEntry && (
+      {chatHistory.length === 0 && <p>⏳ loading...</p>}
+      {chatHistory.length > 0 && (
         <>
-          <Markdown remarkPlugins={[remarkGfm]}>
-            {latestChatEntry.response}
-          </Markdown>
+          {chatHistory.map((chatEntry, index) => (
+            <>
+              <Markdown
+                className={`${styles.chatEntry} zdg-chat-entry-${index}`}
+                remarkPlugins={[remarkGfm]}
+              >
+                {chatEntry.response}
+              </Markdown>
+              {index < chatHistory.length - 1 && (
+                <div className={styles.chatEntryDivider} />
+              )}
+            </>
+          ))}
+          {submittingQuery && <p>⏳ submitting query...</p>}
           <div className={styles.messageBox}>
             <textarea
-              placeholder="Tell me more..."
+              ref={textareaRef}
               onKeyDown={(event) => {
                 if (event.key === "Enter" && !event.shiftKey) {
                   event.preventDefault();
                   console.log("Enter pressed");
+
+                  handleSendMessage();
                 }
               }}
+              placeholder="Tell me more..."
             ></textarea>
-            <button aria-label="Enter">&#9166;</button>
+            <button
+              onClick={handleSendMessage}
+              disabled={submittingQuery}
+              aria-label="Enter"
+            >
+              &#9166;
+            </button>
           </div>
         </>
       )}
