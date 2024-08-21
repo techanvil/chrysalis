6ebbd29b9abad7cf99dd2fe05135e29d69eb5b71
Tom Rees-Herdman <tom@techanvil.co.uk>
2024-08-21 02:07:28 +0100

    Limit auth access to users from specified domains.

--- auth.ts
+++ auth.ts
@@ -1,9 +1,20 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+const ALLOWED_DOMAINS = [
+  "techanvil.co.uk",
+  "10up.com",
+  "get10up.com",
+  "google.com",
+];
+
+function isAllowedDomain(email: string): boolean {
+  const domain = email.split("@")[1];
+  return ALLOWED_DOMAINS.includes(domain);
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
-  /*
   // Lock down the sign-in to only allow users from a specific domain.
   callbacks: {
     async signIn({ account, profile }) {
@@ -12,13 +23,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       }
 
       if (account.provider === "google") {
-        return (
-          !!profile.email_verified && profile.email.endsWith("@techanvil.co.uk")
-        );
+        return !!profile.email_verified && isAllowedDomain(profile.email);
       }
 
       return false; // Do different verification when there are other providers that don't have `email_verified`. Return `false` while there are no other providers.
     },
   },
-  */
 });
